3118844f104ff8d5ecb108bcdd354c091b6e952a
Jony Wesley <lunardo1992@gmail.com>
2022-04-08 20:15:34 -0400

    primeiro commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Buscador
+ 
+ No Front:
+ Utilizei Reactjs e o hook useState  para declarar um estado em um componente criado em formato de função. A função useState recebe por parâmetro o valor padrão do nosso estado e retorna o valor do estado em si e uma função que altera esse valor.
+
+ Na API:
+ Utilizei o Axios ele é um cliente HTTP baseado em Promises para fazer requisições.
---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { useState } from "react";
+import { FiSearch } from "react-icons/fi";
+import "./style.css";
+
+import api from "./Services/api";
+
+function App() {
+  const [input, setInput] = useState("");
+  const [cep, setCep] = useState({});
+
+  async function handleSearch() {
+    if (input === "") {
+      alert("Preencha o cep");
+      return;
+    }
+    try {
+      const response = await api.get(`${input}/json`);
+      setCep(response.data);
+      setInput("");
+    } catch {
+      alert("ops erro ao buscar");
+      setInput("");
+    }
+  }
+
+  return (
+    <div className="container">
+      <h1 className="title">Buscador de CEP</h1>
+
+      <div className="containerInput">
+        <input
+          type="text"
+          placeholder="Digite seu cep..."
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+
+        <button className="buttonSearch" onClick={handleSearch}>
+          <FiSearch size={20} color="#FFF" />
+        </button>
+      </div>
+
+      {Object.keys(cep).length > 0 && (
+        <main className="main">
+          <h2>CEP: {cep.cep}</h2>
+
+          <span>{cep.logradouro}</span>
+          <span>complemento: {cep.complemento}</span>
+          <span>{cep.bairro}</span>
+          <span>
+            {cep.localidade} - {cep.uf}
+          </span>
+        </main>
+      )}
+    </div>
+  );
+}
+
+export default App;
